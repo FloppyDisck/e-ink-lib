fd374751e88a3089301833d64a17af97ed6c937b
Guy S Garcia <34169809+FloppyDisck@users.noreply.github.com>
2020-10-20 19:23:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-	name='eink',
-	packages=['eink'],
+	name='weink',
+	packages=['weink'],
 	version='0.1.0',
 	license='MIT',
 	description='E-ink display library for waveshare screens',
@@ -10,7 +10,7 @@ setup(
 	author_email='guy.garcia@upr.edu',
 	url = 'https://github.com/FloppyDisck/e-ink-lib',
 	download_url='https://github.com/FloppyDisck/e-ink-lib/archive/0.1.0.tar.gz',
-	keywords = ['waveshare', 'eink', 'e-ink'],
+	keywords = ['waveshare', 'eink', 'e-ink', 'w-eink'],
 	install_requires=['Pillow>=6.1.0','RPi.GPIO>=0.6.5','spidev>=3.4'],
 	classifiers=[
 	    'Development Status :: 5 - Production/Stable',
